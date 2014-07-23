3dca2e3a9929d5778f3f5a41bb574e3c279e90fd
otiai10 <otiai10@gmail.com>
2014-07-23 23:13:15 +0900

    Live coding log

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,9 +1,25 @@
+// importするパッケージをさきにコンパイルするひつようがある
 import mysingleton.*;
+import java.util.Date;
 public class Main {
     public static void main (String[] args) {
-        System.out.println("This is main.");
+        Date now = new Date();
+        System.out.println(now);
+
+        // 単一のリソースだったり
+        // リソース管理する単一の入り口だったり
         Singleton theInstance;
         theInstance = Singleton.getInstance();
-        System.out.printf(theInstance.hoge);
+        System.out.println(theInstance.initializedAt);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // e
+        }
+
+        Singleton anotherInstance = Singleton.getInstance();
+        // anotherInstance = new Singleton("piyopiyo");
+        System.out.println(anotherInstance.initializedAt);
     }
 }
--- Singleton/src/mysingleton/Singleton.java
+++ Singleton/src/mysingleton/Singleton.java
@@ -1,18 +1,37 @@
+// packageがさいしょじゃないといけないっぽい？
 package mysingleton;
+
+import java.util.Date;
+
 public class Singleton {
+    // ここがみそ
     private static Singleton instance;
-    private Singleton() {}
+    // private Singleton() {}
     public String hoge;
-    Singleton(String x) {
+    public Date initializedAt;
+
+    // TODO: otiai10
+    // プロセスとはなにか、スレッドとはなにか
+
+    // TODO: otiai10
+    // singletonディスりについてしらbてくる
+
+    // TODO: otiai10
+    // 先生ほしい 
+
+    // defaultだと、同じパッケーじからアクセスできるので
+    // default Singleton(String x) {
+    private Singleton(String x) {
         this.hoge = x;
+        this.initializedAt = new Date();
     }
+
+    // getInstanceでsynchronizedするべきか
+    // Classのロード時に先にinstanceかしといちゃうか
     public static synchronized Singleton getInstance() {
         if (Singleton.instance == null) {
             Singleton.instance = new Singleton("foo");
         }
         return Singleton.instance;
-    }
-    public static void main(String[] args) {
-        System.out.println("Hi, this is singleton demo.");
     }
 }
